ddc07d0d815370290eb8c287a864c6c3fa56cea0
Miruna <elenaned177@gmail.com>
2021-09-14 17:28:54 +0200

    trying

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+
 
 function App() {
+  const [cataloge, setListCataloge] = useState([{}])
+  const [count, setCount] = useState(2)
+
+  const urlApi = `https://swapi.dev/api/people/?page=${count}&format=json`;
+
+
+  //GET the cataloge
+  async function getPostData() {
+    const response = await fetch(urlApi);
+    const data = await response.json();
+    setListCataloge(data.results)
+  }
+
+  //when the user enters the page
+  useEffect(() => {
+    getPostData()
+  }, [cataloge])
+
   return (
     <div className="App">
-      <h1>Hello there!</h1>
-      <button onClick={() => (alert("Hello world!"))} data-cy="btn-hello">Click me!</button>
+      <h1>Star Wars Cataloge</h1>
+      {cataloge && cataloge.map(character => {
+        return (
+          <div >
+            <p>{character.name}</p>
+            <p>{character.height}</p>
+          </div>
+
+        )
+      })}
+      <button onClick={() => {
+        if (count <= 8) {
+          setCount(count + 1)
+          console.log(count)
+        } else {
+          setCount(2)
+        }
+
+      }}>Load More Cards</button>
     </div>
   );
 }
 
 export default App;
+
